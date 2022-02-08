fc330435aa2182e68222adf5351a3926ae40b6a5
Lauren Ferrett <93666073+LFerrett@users.noreply.github.com>
2022-02-08 10:18:41 -0500

    Code cleanup

--- models/Thought.js
+++ models/Thought.js
@@ -1,62 +1,21 @@
 const { Schema, model, Types } = require("mongoose");
 
 const ReactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 1,
-      maxlength: 280,
-    },
-    username: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
+  { reactionId: {type: Schema.Types.ObjectId, default: () => new Types.ObjectId(),},
+    reactionBody: {type: String, required: true, trim: true, minlength: 1, maxlength: 280,},
+    username: {type: String, required: true, trim: true,},
+    createdAt: {type: Date, default: Date.now,},
   },
-  {
-    toJSON: {
-      getters: true,
-    },
-  }
+  { toJSON: {getters: true,},}
 );
 
 const ThoughtSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    thoughtText: {
-      type: String,
-      required: true,
-      minlength: 1,
-      maxlength: 280,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) => dateFormat(createdAtVal),
-    },
+  { username: {type: String, required: true, trim: true, },
+    thoughtText: {type: String, required: true, minlength: 1, maxlength: 280,},
+    createdAt: {type: Date, default: Date.now,},
     reactions: [ReactionSchema],
   },
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true,
-    },
-    id: false,
-  }
+  { toJSON: {virtuals: true, getters: true, }, id: false,}
 );
 
 ThoughtSchema.virtual("reactionCount").get(function () {
--- models/User.js
+++ models/User.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, min: 1, max: 280},
+  username: {type: String, required: true, min: 1, max: 280},
   email: {type: String, required: true, unique: true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please enter a valid email address"],},
-  thoughts: [{type: mongoose.Schema.Types.ObjectId, ref: 'Thought'},],
-  friends: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'},],
+  thoughts: [{type: mongoose.Schema.Types.ObjectId, ref: "Thought"},],
+  friends: [{type: mongoose.Schema.Types.ObjectId, ref: "User"},],
 });
 
-userSchema.virtual('friendCount').get(function() {
-  return this.friends.length || 'Time to go make some friends!';
+userSchema.virtual("friendCount").get(function() {
+  return this.friends.length || "Time to go make some friends!";
 });
 
-const User = model('user', userSchema);
+const User = model("user", userSchema);
 
 module.exports = User;

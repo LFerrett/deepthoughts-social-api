a1bfc9c8a5c3fb545a937dac3901c614fa50fe76
Lauren Ferrett <93666073+LFerrett@users.noreply.github.com>
2022-02-07 23:58:54 -0500

    Moved Reaction model into Thought.

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 const User = require('./User');
 const User = require('./Thought');
-const User = require('./Reaction');
 
-module.exports = { User, Thought, Reaction };
+module.exports = { User, Thought };
